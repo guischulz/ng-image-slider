750cf40d942a719172a12b6167d441dacd014a86
Guido Schulz <guischulz@users.noreply.github.com>
2022-02-19 09:10:21 +0100

    Update build and example demo

--- projects/ng-image-slider/src/public_api.ts
+++ projects/ng-image-slider/src/public_api.ts
@@ -6,3 +6,4 @@ export * from './lib/ng-image-slider.service';
 export * from './lib/ng-image-slider.component';
 export * from './lib/ng-image-slider.module';
 export * from  './lib/model/image.model';
+export * from  './lib/directives/swipe.directive';
